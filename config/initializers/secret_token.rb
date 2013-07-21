6e6ace9443699ffd3150cf86ab51e98945c56e51
bobbyworx <bogdan.popa.a@gmail.com>
2013-07-21 15:10:22 +0300

    techhub commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Techapp::Application.config.secret_token = 'ee810f7710cfa42248d32e52d9f442364bcd33a26f99d8434e2fcbfaa3f91df5a59dc300e72d83852fd5b657f84dca0f634851d2a221d9bc08e02ddb6c5e2eb8'
